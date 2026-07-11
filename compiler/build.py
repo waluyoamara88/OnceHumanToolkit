4020c889b4e727bdcac50da8dbc6071e667d5571
waluyoamara88 <waluyoamara88gmail.com>
2026-07-11 13:40:31 +0700

    feat(compiler): initialize compiler architecture

---
+++ compiler/build.py
@@ -0,0 +1,10 @@
+﻿from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+
+print("="*60)
+print("OnceHumanToolkit Compiler v0.1")
+print("="*60)
+
+print("Project :", ROOT.name)
+print("Status  : Ready")
